08894e0eb06af34e5a90096486f01029c89ffca4
Giorgio Zanni <g.zanni@hotmail.com>
2019-07-29 10:15:44 -0400

    ADD: assets

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,12 +1,14 @@
 import React from "react";
+import logo from "../../assets/images/Logo_ML@2x.png";
+import glassIcon from "../../assets/images/ic_Search.png";
 
 const Navbar = () => (
   <nav>
-    <img src="" alt="" />
+    <img src={logo} alt="" />
     <form>
       <input type="text" />
       <button type="submit">
-        <img src="" alt="" />
+        <img src={glassIcon} alt="Search" />
       </button>
     </form>
   </nav>
